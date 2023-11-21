2f5a389eb3409a3a65196bf405c7fbe91d52e38e
Eduardo Ferreira <eduardo@ocdigital.com.br>
2023-11-21 02:17:55 -0300

    Add nomes Faker

--- database/factories/ProdutoFactory.php
+++ database/factories/ProdutoFactory.php
@@ -16,8 +16,41 @@ class ProdutoFactory extends Factory
      */
     public function definition(): array
     {
+        $nomes = [
+            'Pastel de Carne',
+            'Pastel de Frango com Catupiry',
+            'Pastel de Queijo',
+            'Pastel de Palmito',
+            'Pastel de Camarão',
+            'Pastel de Chocolate',
+            'Pastel de Bacalhau',
+            'Pastel de Calabresa',
+            'Pastel de Milho',
+            'Pastel de Banana',
+            'Café Expresso',
+            'Chá de Camomila',
+            'Suco de Laranja',
+            'Refrigerante de Cola',
+            'Água Mineral',
+            'Vinho Tinto',
+            'Cerveja Artesanal',
+            'Margarita',
+            'Mojito',
+            'Smoothie de Frutas',
+            'Coxinha de Frango',
+            'Empada de Palmito',
+            'Quibe Frito',
+            'Bolinha de Queijo',
+            'Esfiha de Carne',
+            'Croquete de Batata',
+            'Enroladinho de Salsicha',
+            'Pastel de Camarão',
+            'Bolinho de Bacalhau',
+            'Rissole de Milho'
+        ];
+
         return [
-            'nome' => $this->faker->name(),
+            'nome' => $this->faker->randomElement($nomes),
             'preco' => $this->faker->randomFloat(2, 0, 9.99),
             'foto' => $this->faker->imageUrl(640, 480),
         ];
